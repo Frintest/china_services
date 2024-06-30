5fad3bd093329fcc09b303c7a37e4a014f1899a1
Dmitry <dimaruzaev2@gmai.com>
2024-06-30 16:32:03 +0300

    v1.5

--- app/keyboards.py
+++ app/keyboards.py
@@ -28,7 +28,7 @@ cargo4 = InlineKeyboardMarkup(inline_keyboard=[[
 cargo5 = InlineKeyboardMarkup(inline_keyboard=[
     [
         InlineKeyboardButton(text="Делаем обрешетку", callback_data="cargo/5/box"),
-        InlineKeyboardButton(text="Делаем доп. скотч", callback_data="cargo/5/scotch")
+        InlineKeyboardButton(text="Делаем доп. скотч", callback_data="cargo/5/scotch"),
     ],
     [
         InlineKeyboardButton(text="Не делаем доп. упаковку", callback_data="cargo/5/null"),
@@ -37,4 +37,23 @@ cargo5 = InlineKeyboardMarkup(inline_keyboard=[
 
 cargo6 = InlineKeyboardMarkup(inline_keyboard=[[
     InlineKeyboardButton(text="Я не уверен, помогите!", callback_data="cargo/6/help"),
+]])
+
+retail1 = InlineKeyboardMarkup(inline_keyboard=[[
+    InlineKeyboardButton(text="Продолжаем", callback_data="retail/1/countinue"),
+]])
+
+retail2 = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Рассчитать стоимость товара", callback_data="retail/2/price")],
+    [InlineKeyboardButton(text="Рассчитать доставку", callback_data="retail/2/delivery")]
+])
+
+retail3 = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Рассчитать доставку", callback_data="retail/3/delivery")],
+    [InlineKeyboardButton(text="Я просчитал(а) стоимость товара и доставки и готов(а) делать заказ",
+                          callback_data="retail/3/all")]
+])
+
+retail4 = InlineKeyboardMarkup(inline_keyboard=[[
+    InlineKeyboardButton(text="КАК ПРИСЛАТЬ ССЫЛКУ?", callback_data="retail/4/link"),
 ]])
